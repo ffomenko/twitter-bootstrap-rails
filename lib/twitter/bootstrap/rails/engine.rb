3a9df352fe26f524bfcbc38b1d5a637f812c824d
Fedor Fomenko <ffomenko@yahoo.co.uk>
2012-11-13 07:06:35 -0800

    More fixes

--- lib/twitter/bootstrap/rails/engine.rb
+++ lib/twitter/bootstrap/rails/engine.rb
@@ -17,7 +17,7 @@ module Twitter
 
         initializer 'twitter-bootstrap-rails.setup_helpers' do |app|
           app.config.to_prepare do
-            ActionController::Base.send :include, BreadCrumbs
+            # ActionController::Base.send :include, BreadCrumbs
             ActionController::Base.send :helper, FlashBlockHelper
             ActionController::Base.send :helper, ModalHelper
             #ActionController::Base.send :helper_method, :render_breadcrumbs
